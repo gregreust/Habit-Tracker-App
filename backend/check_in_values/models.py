785ab4872041d59598360dae386e6c96c7ae8ad2
gregreust <greg.reust@gmail.com>
2022-11-11 11:30:08 -0600

    Cleaned up unneeded files and created Django app and model for check_in_values

---
+++ backend/check_in_values/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+class CheckInValues (models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    check_in_1 = models.IntegerField()
+    check_in_2 = models.IntegerField()
+    check_in_3 = models.IntegerField()
+    check_in_4 = models.IntegerField()
+    check_in_5 = models.IntegerField()
+    check_in_6 = models.IntegerField()
+    check_in_7 = models.IntegerField()
+
+
+
