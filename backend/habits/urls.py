d45fbcf8d261772672b1d17d28ede96c40083139
gregreust <greg.reust@gmail.com>
2022-11-13 23:57:14 -0600

    View functions for adding and removing a user's tracked habits are tested and functional

--- backend/habits/urls.py
+++ backend/habits/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_user_habits),
-    path('<int:habit_id>', views.remove_user_habit)
+    path('', views.user_habits),
+    path('<int:habit_id>/', views.add_or_remove_user_habit),
 ]
